8a250218d988fccd20d155e54a9b6a4195abe46f
misscs <admin@Admins-MacBook.local>
2011-01-06 10:09:39 -0500

    laying in the basic theming concept. major works needs to be done editing the content, especially blogs, to standardize nicely on homepage. need to add @font-face to headers. grid classes to skinr

--- template.php
+++ template.php
@@ -1,6 +1,14 @@
 <?php
 
 /**
- * 
- * 
+ * Override or insert variables into the block templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("block" in this case.)
  */
+function sled_preprocess_block(&$vars, $hook) {
+  // Special classes for blocks.
+  $vars['title_attributes_array']['class'][] = 'block-title';
+}
